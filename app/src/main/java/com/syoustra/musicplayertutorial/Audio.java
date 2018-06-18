d9f558e603b1ee568cc935fb5885ca4da30f4ec6
Stephanie Youstra <steph.you07@gmail.com>
2018-06-18 18:08:33 -0400

    Create Audio model

---
+++ app/src/main/java/com/syoustra/musicplayertutorial/Audio.java
@@ -0,0 +1,52 @@
+package com.syoustra.musicplayertutorial;
+
+import java.io.Serializable;
+
+//TODO 17. Create Audio class to set the stage for playing media from device
+
+public class Audio implements Serializable {
+    private String data;
+    private String title;
+    private String album;
+    private String artist;
+
+    public Audio(String data, String title, String album, String artist) {
+        this.data = data;
+        this.title = title;
+        this.album = album;
+        this.artist = artist;
+    }
+
+    public String getData() {
+
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}

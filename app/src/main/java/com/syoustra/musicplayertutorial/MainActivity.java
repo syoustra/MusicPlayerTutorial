134db26d54e7befb1b658ac4b55a350c59a83613
Stephanie Youstra <steph.you07@gmail.com>
2018-06-18 03:37:07 -0400

    Bind Service to MainActivity

--- app/src/main/java/com/syoustra/musicplayertutorial/MainActivity.java
+++ app/src/main/java/com/syoustra/musicplayertutorial/MainActivity.java
@@ -1,7 +1,12 @@
 package com.syoustra.musicplayertutorial;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.media.MediaPlayer;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /** MEDIA PLAYER:
  *
@@ -13,9 +18,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO 13. Add global variables to bind mediaPlayer and Main Activity
+    private MediaPlayerService player;
+    boolean serviceBound = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Binding this Client to the AudioPlayer Service
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //We've bound to LocalService, cast the IBinder and get LocalService instance
+            MediaPlayerService.LocalBinder binder = (MediaPlayerService.LocalBinder) service;
+            player = binder.getService();
+            serviceBound = true;
+
+            Toast.makeText(MainActivity.this, "Service Bound", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
+
+
 }

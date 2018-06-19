287bb69ecf2af04d2b7a878f6ddacbc1f8f9bc4c
Stephanie Youstra <steph.you07@gmail.com>
2018-06-19 04:40:06 -0400

    Create StorageUtil to store the song list in SharedPreferences

---
+++ app/src/main/java/com/syoustra/musicplayertutorial/StorageUtil.java
@@ -0,0 +1,60 @@
+package com.syoustra.musicplayertutorial;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+//TODO 30. Create StorageUtil class to hold the song list in SharedPreferences
+public class StorageUtil {
+
+    private final String STORAGE = "com.syoustra.musicplayertutorial.STORAGE";
+    private SharedPreferences preferences;
+    private Context context;
+
+    public StorageUtil(Context context) {
+        this.context = context;
+    }
+
+    public void storeAudio(ArrayList<Audio> arrayList) {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(arrayList);
+        editor.putString("audioArrayList", json);
+        editor.apply();
+    }
+
+    public ArrayList<Audio> loadAudio() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = preferences.getString("audioArrayList", null);
+        Type type = new TypeToken<ArrayList<Audio>>() {
+            }.getType();
+        return gson.fromJson(json, type);
+    }
+
+    public void storeAudioIndex(int index) {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("audio index", index);
+        editor.apply();
+    }
+
+    public int loadAudioIndex() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        return preferences.getInt("audioIndex", -1); //return -1 if no data is found
+    }
+
+    public void clearCachedAudioPlaylist() {
+        preferences = context.getSharedPreferences(STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+}
